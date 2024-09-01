4091481858cd2f23fa85647f371f23efd1376a64
dani_edo <dani.edogawa.17@gmail.com>
2024-09-01 12:53:02 +0700

    new item widget

---
+++ lib/widget/new_item.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class NewItem extends StatefulWidget {
+  const NewItem({super.key});
+
+  @override
+  _NewItemState createState() => _NewItemState();
+}
+
+class _NewItemState extends State<NewItem> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
