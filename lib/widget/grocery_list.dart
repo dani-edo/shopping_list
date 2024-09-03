4488c005da2c6531488731158323973a21631eaf
dani_edo <dani.edogawa.17@gmail.com>
2024-09-03 11:47:00 +0700

    handling error null on empty list

--- lib/widget/grocery_list.dart
+++ lib/widget/grocery_list.dart
@@ -41,6 +41,13 @@ class _GroceryListState extends State<GroceryList> {
       return;
     }
 
+    if (response.body == 'null') {
+      setState(() {
+        _isLoading = false;
+      });
+      return;
+    }
+
     final Map<String, dynamic> listData = json.decode(response.body);
     final List<GroceryItem> _loadedItems = [];
     for (final item in listData.entries) {
